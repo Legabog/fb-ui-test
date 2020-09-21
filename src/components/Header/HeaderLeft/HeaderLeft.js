f7a03e9a3338fbdd3130276e15f312e5d1facf57
legabog <probringer@mail.ru>
2020-09-21 18:00:14 +0300

    Add files via upload

--- src/components/Header/HeaderLeft/HeaderLeft.js
+++ src/components/Header/HeaderLeft/HeaderLeft.js
@@ -1,13 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 import HeaderInput from "../../common/HeaderInput/HeaderInput";
 import HeaderLogo from "../../common/HeaderLogo/HeaderLogo";
 import "./HeaderLeft.css";
+import HeaderActiveCovidSection from "./HeaderActiveCovidSection/HeaderActiveCovidSection";
 
 const HeaderLeft = (props) => {
-  return (
+  const [activeInput, setActiveInput] = useState(false);
+
+  const toggleActiveInput = () => {
+    activeInput ? setActiveInput(false) : setActiveInput(true);
+  };
+
+  return activeInput ? (
+    <div className="header__left__active">
+      <div className="header__left__active__head">
+        <HeaderLogo
+          activeInput={activeInput}
+          toggleActiveInput={toggleActiveInput}
+        />
+        <HeaderInput
+          activeInput={activeInput}
+          toggleActiveInput={toggleActiveInput}
+        />
+      </div>
+      <div className="header__left__active__body">
+        <HeaderActiveCovidSection />
+      </div>
+    </div>
+  ) : (
     <div className="header__left">
-      <HeaderLogo />
-      <HeaderInput />
+      <HeaderLogo
+        activeInput={activeInput}
+        toggleActiveInput={toggleActiveInput}
+      />
+      <HeaderInput
+        activeInput={activeInput}
+        toggleActiveInput={toggleActiveInput}
+      />
     </div>
   );
 };
