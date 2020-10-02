b769c18ed288f4ae8a0d5b94f65d64fb88e938c0
legabog <probringer@mail.ru>
2020-10-02 18:36:08 +0300

    Add files via upload

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -2,12 +2,14 @@ import React from "react";
 import ProfileUpdateAvatar from "../ProfileUpdateAvatar/ProfileUpdateAvatar";
 import "./Profile.css";
 import ProfileHeader from "./ProfileHeader/ProfileHeader";
+import ProfileSelectAvatarBackground from "../ProfileSelectAvatarBackground/ProfileSelectAvatarBackground";
 
 const Profile = (props) => {
   return (
     <div className={"profile"}>
       <ProfileHeader {...props} />
       <ProfileUpdateAvatar {...props} />
+      <ProfileSelectAvatarBackground {...props} />
       <div className={"profile__body"}></div>
     </div>
   );
