885200df4a335d2e93db3d2b6da455d962024fae
legabog <probringer@mail.ru>
2020-10-02 18:39:48 +0300

    Add files via upload

--- src/components/Profile/ProfileHeader/ProfileHeaderAvatarBackgroundWindow/ProfileHeaderAvatarBackgroundWindow.js
+++ src/components/Profile/ProfileHeader/ProfileHeaderAvatarBackgroundWindow/ProfileHeaderAvatarBackgroundWindow.js
@@ -1,6 +1,7 @@
 import React from "react";
 import PhotoLibraryIcon from "@material-ui/icons/PhotoLibrary";
 import PublishIcon from "@material-ui/icons/Publish";
+import DeleteIcon from "@material-ui/icons/Delete";
 import "./ProfileHeaderAvatarBackgroundWindow.css";
 
 const ProfileHeaderAvatarBackgroundWindow = (props) => {
@@ -13,7 +14,16 @@ const ProfileHeaderAvatarBackgroundWindow = (props) => {
       }}
     >
       <div className={"profile__header__avatarBackground__window"}>
-        <div className={"profile__header__avatarBackground__windowSection"}>
+        <div
+          className={"profile__header__avatarBackground__windowSection"}
+          onClick={() => {
+            props.toggleProfileSelectAvatarBackground(
+              props.profileSelectVisibility,
+              props.profileSelectOpacity
+            );
+            props.toggleWindowAvatarBackground();
+          }}
+        >
           <PhotoLibraryIcon />
           <span>Select Photo</span>
         </div>
@@ -22,12 +32,24 @@ const ProfileHeaderAvatarBackgroundWindow = (props) => {
           <div
             className={"profile__header__avatarBackground__windowSection"}
             onClick={props.toggleWindowAvatarBackground}
-            
           >
             <PublishIcon />
             <span>Upload Photo</span>
           </div>
         </label>
+
+        {props.user === null || props.user.AvatarBackground === "" ? null : (
+          <div
+            className={"profile__header__avatarBackground__windowSection"}
+            onClick={() => {
+              props.toggleConfirmRemove(true);
+              props.toggleWindowAvatarBackground();
+            }}
+          >
+            <DeleteIcon />
+            <span>Remove</span>
+          </div>
+        )}
       </div>
     </div>
   );
