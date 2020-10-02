c0f9c1cc98d4406e57a6cba2465ef8ccc7600ac2
legabog <probringer@mail.ru>
2020-10-02 18:39:09 +0300

    Add files via upload

--- src/components/Profile/ProfileHeader/ProfileHeaderButtonAddBackground/ProfileHeaderButtonAddBackground.js
+++ src/components/Profile/ProfileHeader/ProfileHeaderButtonAddBackground/ProfileHeaderButtonAddBackground.js
@@ -6,6 +6,9 @@ const ProfileHeaderButtonAddBackground = (props) => {
   return (
     <div
       className={"profile__header__buttonAddCover"}
+      style={{
+        display: props.windowConfirmBackgroundOpacity === 1 ? "none" : null,
+      }}
       onClick={props.toggleWindowAvatarBackground}
     >
       <PhotoCameraIcon />
