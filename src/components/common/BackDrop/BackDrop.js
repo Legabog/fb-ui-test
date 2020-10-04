8413e2ca32f2bb8b98273d2af6a81f3b48633a9d
legabog <probringer@mail.ru>
2020-10-04 19:06:24 +0300

    Add files via upload

--- src/components/common/BackDrop/BackDrop.js
+++ src/components/common/BackDrop/BackDrop.js
@@ -2,7 +2,11 @@ import React from "react";
 import "./BackDrop.css";
 
 const BackDrop = (props) => (
-  <div className="backdrop" onClick={props.onClick}></div>
+  <div
+    className="backdrop"
+    style={{ zIndex: `${props.zIndex}` }}
+    onClick={props.onClick}
+  ></div>
 );
 
 export default BackDrop;
